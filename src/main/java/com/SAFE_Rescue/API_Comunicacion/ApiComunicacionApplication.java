1beb90226139d5254f3786f44e26881d6acaf3dd
Rubén Parada Peña <nico.94lta@gmail.com>
2025-05-24 23:02:50 -0400

    Inicializacion API-Comunicacion

---
+++ src/main/java/com/SAFE_Rescue/API_Comunicacion/ApiComunicacionApplication.java
@@ -0,0 +1,13 @@
+package com.SAFE_Rescue.API_Comunicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComunicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComunicacionApplication.class, args);
+	}
+
+}
